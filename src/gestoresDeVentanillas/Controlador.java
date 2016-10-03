7403e2e0417f64b0fcd40c03dd33068a4aeded37
Milka <Milka@Cabrilla>
2016-10-03 14:50:51 -0500

    nuevos paquetes

--- src/gestoresDeVentanillas/Controlador.java
+++ src/gestoresDeVentanillas/Controlador.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto;
+package gestoresDeVentanillas;
 
 import java.util.ArrayList;
+import consultorio.Paciente;
+import consultorio.Persona;
 
 /**
  *
