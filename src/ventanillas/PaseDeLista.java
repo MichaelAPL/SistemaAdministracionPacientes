abcd5ab8cc15463ab5430550ab67834091aef84a
Milka <Milka@Cabrilla>
2016-10-03 20:49:10 -0500

    clase pase lista

---
+++ src/ventanillas/PaseDeLista.java
@@ -0,0 +1,34 @@
+
+package ventanillas;
+
+import java.util.ArrayList;
+import consultorio.*;
+import javax.swing.JCheckBox;
+
+/**
+ *
+ * @author Milka
+ */
+public class PaseDeLista {
+    ArrayList<Paciente> pacientes;
+    VentanillaVPaseDeLista ventana;
+    
+    public PaseDeLista(ArrayList<Paciente> pacientes){
+        this.pacientes = pacientes;
+        ventana = new VentanillaVPaseDeLista();
+        crearPaseDeLista();
+        ventana.setVisible(true);
+    }
+    
+    private void crearPaseDeLista(){
+        for (int i = 0; i < pacientes.size(); i++) {
+            JCheckBox checkBox = new JCheckBox();
+            checkBox.setText(pacientes.get(i).getNombres()+" "+pacientes.get(i).getApellidos()+" -- Tipo:"+pacientes.get(i).getSiguienteCita()+" - EDTA:"+pacientes.get(i).getDosis_ml_EDTA());
+            ventana.add(checkBox);
+        }
+    }
+    
+    
+    
+    
+}
