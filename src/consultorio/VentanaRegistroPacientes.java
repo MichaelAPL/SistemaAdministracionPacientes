ed7d91b9cd8802d62f8f3c5e30f3a517f3edb661
Miguel Pat <michaelapl96@gmail.com>
2016-10-04 18:15:25 -0500

    Modificados: VentanaRegistroPacientes, ControladorRegistro, se eliminó la clase TablaRequerimientos

--- src/consultorio/VentanaRegistroPacientes.java
+++ src/consultorio/VentanaRegistroPacientes.java
@@ -5,8 +5,9 @@
  */
 package consultorio;
 
-import gestoresDeVentanillas.Controlador;
+import gestoresDeVentanillas.ControladorRegistro;
 import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -17,20 +18,20 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
     /**
      * Creates new form VentanaRegistroPacientes
      */
-    TablaPadecimientos datosPadecimientos = new TablaPadecimientos();
-    Controlador controlador;
     
-    public VentanaRegistroPacientes(Controlador controlador) {
+    
+    private ControladorRegistro controladorRegistro;
+    private ArrayList<String> padecimientosPrevios;
+    private ArrayList<String> medicamentosExternos;
+    
+    public VentanaRegistroPacientes(ControladorRegistro controladorRegistro) {
         initComponents();
-        this.controlador = controlador;
+        this.controladorRegistro = controladorRegistro;
+        inicializarTablaPadecimientos();
+        inicializarTablaMedicamentos();
         setLocationRelativeTo(null);
-        datosPadecimientos.inicializarTabla(tablaPadecimientos);
         this.setVisible(true);
-    }
-    
-    
-    
-    
+    }   
 
 
     @SuppressWarnings("unchecked")
@@ -57,6 +58,10 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
         nombres = new javax.swing.JTextField();
         botonGuardar = new javax.swing.JButton();
         edad = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        medicamento = new javax.swing.JTextField();
+        scrollTabla1 = new javax.swing.JScrollPane();
+        tablaMedicamentos = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -120,6 +125,27 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setText("Medicamentos Alternos");
+
+        medicamento.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                medicamentoKeyPressed(evt);
+            }
+        });
+
+        tablaMedicamentos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        scrollTabla1.setViewportView(tablaMedicamentos);
+
         javax.swing.GroupLayout panelLayout = new javax.swing.GroupLayout(panel);
         panel.setLayout(panelLayout);
         panelLayout.setHorizontalGroup(
@@ -132,45 +158,49 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(direccion))
                     .addGroup(panelLayout.createSequentialGroup()
+                        .addComponent(etiquetaNombre)
+                        .addGap(18, 18, 18)
+                        .addComponent(nombres, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addComponent(etiquetaLocalidad)
+                        .addGap(18, 18, 18)
+                        .addComponent(localidad, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(74, 74, 74)
+                        .addComponent(etiquetaTelefono)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(telefono, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addComponent(etiquetaDosis)
+                        .addGap(18, 18, 18)
+                        .addComponent(EDTA, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(etiquetaApellidos)
+                            .addComponent(etiquetaEdad))
                         .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(panelLayout.createSequentialGroup()
-                                .addComponent(etiquetaNombre)
                                 .addGap(18, 18, 18)
-                                .addComponent(nombres, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(apellidos, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(panelLayout.createSequentialGroup()
-                                .addComponent(etiquetaLocalidad)
-                                .addGap(18, 18, 18)
-                                .addComponent(localidad, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(74, 74, 74)
-                                .addComponent(etiquetaTelefono)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(telefono, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(panelLayout.createSequentialGroup()
-                                .addComponent(etiquetaDosis)
-                                .addGap(18, 18, 18)
-                                .addComponent(EDTA, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(panelLayout.createSequentialGroup()
-                                .addComponent(etiquetaPadecimientos)
-                                .addGap(18, 18, 18)
-                                .addComponent(padecimiento, javax.swing.GroupLayout.PREFERRED_SIZE, 299, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(scrollTabla, javax.swing.GroupLayout.PREFERRED_SIZE, 626, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(panelLayout.createSequentialGroup()
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(etiquetaApellidos)
-                                    .addComponent(etiquetaEdad))
-                                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(panelLayout.createSequentialGroup()
-                                        .addGap(18, 18, 18)
-                                        .addComponent(apellidos, javax.swing.GroupLayout.PREFERRED_SIZE, 271, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(panelLayout.createSequentialGroup()
-                                        .addGap(19, 19, 19)
-                                        .addComponent(edad, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                        .addGap(0, 37, Short.MAX_VALUE)))
-                .addContainerGap(37, Short.MAX_VALUE))
+                                .addGap(19, 19, 19)
+                                .addComponent(edad, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addComponent(etiquetaPadecimientos)
+                        .addGap(4, 4, 4)
+                        .addComponent(padecimiento, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(medicamento, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelLayout.createSequentialGroup()
+                        .addComponent(scrollTabla, javax.swing.GroupLayout.PREFERRED_SIZE, 385, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(scrollTabla1, javax.swing.GroupLayout.PREFERRED_SIZE, 408, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
             .addGroup(panelLayout.createSequentialGroup()
-                .addGap(286, 286, 286)
+                .addGap(380, 380, 380)
                 .addComponent(botonGuardar)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         panelLayout.setVerticalGroup(
             panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -202,14 +232,18 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
                     .addComponent(etiquetaDosis)
                     .addComponent(EDTA, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(24, 24, 24)
-                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(etiquetaPadecimientos)
-                    .addComponent(padecimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addComponent(scrollTabla, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(botonGuardar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(panelLayout.createSequentialGroup()
+                        .addGroup(panelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(etiquetaPadecimientos)
+                            .addComponent(padecimiento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1)
+                            .addComponent(medicamento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(scrollTabla, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(scrollTabla1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addComponent(botonGuardar))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -232,16 +266,17 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void botonGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonGuardarActionPerformed
-        ArrayList arregloDeDatos = new ArrayList();
-        arregloDeDatos.add(nombres.getText());
-        arregloDeDatos.add(apellidos.getText());
-        arregloDeDatos.add(edad.getText());
-        arregloDeDatos.add(direccion.getText());
-        arregloDeDatos.add(localidad.getText());
-        arregloDeDatos.add(telefono.getText());
-        arregloDeDatos.add(EDTA.getText());
-        arregloDeDatos.add(datosPadecimientos.getPadecimientos());
-        controlador.crearNuevoPaciente(arregloDeDatos);
+        ArrayList datosDelPaciente = new ArrayList();
+        datosDelPaciente.add(nombres.getText());
+        datosDelPaciente.add(apellidos.getText());
+        datosDelPaciente.add(edad.getText());
+        datosDelPaciente.add(direccion.getText());
+        datosDelPaciente.add(localidad.getText());
+        datosDelPaciente.add(telefono.getText());
+        datosDelPaciente.add(EDTA.getText());
+        datosDelPaciente.add(this.padecimientosPrevios);
+        datosDelPaciente.add(this.medicamentosExternos);
+        controladorRegistro.crearNuevoPaciente(datosDelPaciente);
     }//GEN-LAST:event_botonGuardarActionPerformed
 
     private void edadKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_edadKeyTyped
@@ -263,7 +298,27 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
         char caracter = evt.getKeyChar();
     }//GEN-LAST:event_padecimientoKeyPressed
 
+    private void medicamentoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_medicamentoKeyPressed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_medicamentoKeyPressed
 
+    private void inicializarTablaPadecimientos(){
+        this.padecimientosPrevios = new ArrayList();
+        DefaultTableModel modelo;
+        String cabecera[] = {"Padecimientos previos"};
+        String datos[][] = {};
+        modelo = new DefaultTableModel(datos, cabecera);
+        this.tablaPadecimientos.setModel(modelo);
+    }
+    
+    private void inicializarTablaMedicamentos(){
+        this.medicamentosExternos = new ArrayList();
+        DefaultTableModel modelo;
+        String cabecera[] = {"Medicamentos externos"};
+        String datos[][] = {};
+        modelo = new DefaultTableModel(datos, cabecera);
+        this.tablaMedicamentos.setModel(modelo);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField EDTA;
@@ -279,11 +334,15 @@ public class VentanaRegistroPacientes extends javax.swing.JFrame {
     private javax.swing.JLabel etiquetaNombre;
     private javax.swing.JLabel etiquetaPadecimientos;
     private javax.swing.JLabel etiquetaTelefono;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField localidad;
+    private javax.swing.JTextField medicamento;
     private javax.swing.JTextField nombres;
     private javax.swing.JTextField padecimiento;
     private javax.swing.JPanel panel;
     private javax.swing.JScrollPane scrollTabla;
+    private javax.swing.JScrollPane scrollTabla1;
+    private javax.swing.JTable tablaMedicamentos;
     private javax.swing.JTable tablaPadecimientos;
     private javax.swing.JTextField telefono;
     // End of variables declaration//GEN-END:variables
