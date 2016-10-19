80b68cf78e80ed24303d5dab7a031b8efa9cd43a
Angel Basto Gonzalez <angelbastogz@gmail.com>
2016-10-19 12:58:12 -0500

    TestInventario es para probar el modulo de inventario.

---
+++ src/tests/TestIventario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import vistas.VentanaInventario;
+
+/**
+ *
+ * @author Angel Basto Gonzalez
+ */
+public class TestIventario {
+    
+    public static void main(String[] args) {
+        VentanaInventario ventanaInventario = new VentanaInventario();
+        ventanaInventario.setVisible(true);
+    }   
+}
