4acc3ed9b0c337beacaa576d05197c617eb018b5
Miguel Pat <michaelapl96@gmail.com>
2016-11-05 21:23:51 -0600

    Solucionados conflictos

--- src/modelos/Tratamiento.java
+++ src/modelos/Tratamiento.java
@@ -16,15 +16,10 @@ public class Tratamiento {
     private String clavePaciente;
     private Suero suero;
     private int dosis_EDTA_ml;
-<<<<<<< HEAD
     private int numeroDeSueros;
-    
-    public Tratamiento(String clavePaciente, int dosisEDTA){
-=======
     private int numeroDeSuerosAPlicados;
 
     public Tratamiento(String clavePaciente, int dosisEDTA) {
->>>>>>> 0a450b78ef93a9392ce02fcf29ad3bcc98167ea2
         this.clavePaciente = clavePaciente;
         this.dosis_EDTA_ml = dosisEDTA;
         this.numeroDeSueros = 0;
@@ -45,25 +40,17 @@ public class Tratamiento {
     public int getDosisEDTA() {
         return this.dosis_EDTA_ml;
     }
-<<<<<<< HEAD
-    
-    public Suero sueroAaplicar(){
-        if(this.numeroDeSueros != 0 && 
-            this.numeroDeSueros % 5 == 0){
-=======
 
     //devuelve el tipo de suero a aplicar según el número de suero que toca aplicar
 
     public Suero sueroAaplicar() {
         if (this.numeroDeSuerosAPlicados != 0
             && this.numeroDeSuerosAPlicados % 5 == 0) {
->>>>>>> 0a450b78ef93a9392ce02fcf29ad3bcc98167ea2
             return suero.MINERAL;
         } else {
             return suero.QUELANTE;
         }
     }
-<<<<<<< HEAD
     
     public int getNumeroSuerosAplicados(){
         return this.numeroDeSueros;
@@ -71,14 +58,5 @@ public class Tratamiento {
     
     public void agregarSueroAlHistorial(){
         this.numeroDeSueros ++;
-=======
-
-    public int getNumeroSuerosAplicados() {
-        return this.numeroDeSuerosAPlicados;
-    }
-
-    public void agregarSueroAlHistorial() {
-        this.numeroDeSuerosAPlicados++;
->>>>>>> 0a450b78ef93a9392ce02fcf29ad3bcc98167ea2
     }
 }
