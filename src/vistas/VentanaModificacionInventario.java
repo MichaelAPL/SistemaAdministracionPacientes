79729bd1526a4b857163ec5ed2590bc6f694384e
Angel Basto Gonzalez <angelbastogz@gmail.com>
2016-11-13 19:16:03 -0600

    VentanaModificacionInventario modificada

--- src/vistas/VentanaModificacionInventario.java
+++ src/vistas/VentanaModificacionInventario.java
@@ -5,8 +5,10 @@
  */
 package vistas;
 
+import controladores.ControladorInventario;
 import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
+import modelos.InventarioProducto;
 
 /**
  *
@@ -17,12 +19,14 @@ public class VentanaModificacionInventario extends javax.swing.JFrame {
     /**
      * Creates new form VentanaModificacionInventario
      */
-    public VentanaModificacionInventario() {
+    
+    ControladorInventario controladorInventario = new ControladorInventario();
+    public VentanaModificacionInventario(ControladorInventario controladorInventario) {
         initComponents();
         setLocationRelativeTo(null);
+        this.controladorInventario = controladorInventario;
         insertarOpcionesMenu();
     }
-
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -34,32 +38,38 @@ public class VentanaModificacionInventario extends javax.swing.JFrame {
     private void initComponents() {
 
         producto = new javax.swing.JComboBox();
-        jSpinner1 = new javax.swing.JSpinner();
+        existencias = new javax.swing.JSpinner();
         btnAgregar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Modificación Inventario");
 
         producto.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
 
-        jSpinner1.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
+        existencias.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
+        existencias.setModel(new javax.swing.SpinnerNumberModel(0, 0, 99, 1));
 
         btnAgregar.setFont(new java.awt.Font("Consolas", 0, 14)); // NOI18N
         btnAgregar.setText("Agregar");
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(btnAgregar)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(producto, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(77, Short.MAX_VALUE))
+                        .addComponent(existencias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(76, 76, 76))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -67,7 +77,7 @@ public class VentanaModificacionInventario extends javax.swing.JFrame {
                 .addGap(44, 44, 44)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(producto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(existencias, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(btnAgregar)
                 .addContainerGap(24, Short.MAX_VALUE))
@@ -75,6 +85,15 @@ public class VentanaModificacionInventario extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+        // TODO add your handling code here:
+        String nombreProducto =String.valueOf(producto.getItemAt(producto.getSelectedIndex()));
+        int insumo = Integer.parseInt((String) existencias.getValue());
+        InventarioProducto productoInventario = new InventarioProducto(nombreProducto, insumo);
+        
+        controladorInventario.agregarProductoInventario(productoInventario);
+    }//GEN-LAST:event_btnAgregarActionPerformed
 
     public void insertarOpcionesMenu(){
         producto.addItem("Normogotero");
@@ -88,7 +107,7 @@ public class VentanaModificacionInventario extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAgregar;
-    private javax.swing.JSpinner jSpinner1;
+    private javax.swing.JSpinner existencias;
     private javax.swing.JComboBox producto;
     // End of variables declaration//GEN-END:variables
 }
